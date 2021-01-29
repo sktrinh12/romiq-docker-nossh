db66525b5a0105028e76a71a5c065c52d2fbb3d9
sktrinh12 <trinh.kcs85@gmail.com>
2021-01-28 22:35:13 -0800

    use of subprocess to call Rscript, appending args for Rscript beforehand

--- services/romiq/functions.py
+++ services/romiq/functions.py
@@ -1,5 +1,18 @@
 import subprocess
 import os
+from datetime import datetime
+
+def validate_barcode(barcode):
+    """
+    validate the barcode string using the first 8 characters and converting it to a datetime
+    """
+    str_format = "%Y%m%d"
+    try:
+        datetime.strptime(barcode[:8], str_format)
+        return True
+    except ValueError:
+        return False
+
 
 def run_Rscript(script_name, **kwargs):
     """
@@ -9,8 +22,8 @@ def run_Rscript(script_name, **kwargs):
     # just to see which pyapi server is being used
     # print(f'hostname: {host}')
 
-    username = os.getenv("UNAME")
-    version = os.getenv("VERSION")
+    username = os.getenv("UNAME", "bdb")
+    version = os.getenv("VERSION", "1_2_1b")
     omiq_path = os.path.join("/home", username, "R")
     if script_name == "main_driver":
         rscript_path = os.path.join(omiq_path, f"omiq_v{version}",
@@ -18,28 +31,31 @@ def run_Rscript(script_name, **kwargs):
     else:
         rscript_path = os.path.join(omiq_path, f"{script_name}.R")
 
+    # for testing
+    # rscript_path = '/Users/spencertrinh/GitRepos/romiq-docker-nossh/services/romiq/test.R'
 
     # run rscript
-    cmd = f"/usr/bin/Rscript {rscript_path}"
+    cmd = ["Rscript"]
+    cmd.append(rscript_path)
     barcode = kwargs.get('barcode', '')
     re_run = kwargs.get('re_run', '')
     if barcode:
-        cmd += f" {barcode}"
+        cmd.append(barcode)
     else:
         raise("need to supply barcode argument")
     if re_run not in ['', None]:
-        cmd += f" {re_run}"
+        cmd.append(re_run)
 
     print('='*40)
     print(f'running command: \"{cmd}\"')
     print('='*40)
 
-    process = subprocess.Popen(list(cmd), stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE, shell=True)
-    stdout_lines = ' '.join(prorcess.stdout)
-    print(stdout_lines)
-    if stderr:
-        stderr_lines = ' '.join(process.stderr)
-        print(stderr_lines)
-    return stdout_lines
+    try:
+        process = subprocess.check_output(cmd, universal_newlines=True)
+    except subprocess.SubprocessError as e:
+        err = f'Status: FAIL {e.returncode} {e.output}'
+        print(err)
+        process = err
+    print(process)
+    return process
 
