070c323e2e8650022b55749730a6077ba25c4469
sktrinh12 <trinh.kcs85@gmail.com>
2021-01-28 22:35:29 -0800

    added additional print stms for testing

--- services/romiq/test.R
+++ services/romiq/test.R
@@ -3,9 +3,9 @@ rerun_rd <- FALSE
 
 # test if there is one argument
 if (length(args) == 0) {
-    stop("At least one argument must be supplied (timestamp).n", call.=FALSE)
+    stop("At least one argument must be supplied (barcode).n", call.=FALSE)
 } else if (length(args) == 2) {
-    if (match(tolower(args[2]), "true") | tolower(args[2]) == "t") { 
+    if (as.logical(toupper(args[2])) == TRUE) {
         rerun_rd <- TRUE
     }
 }
@@ -13,3 +13,9 @@ if (length(args) == 0) {
 print(R.Version())
 print(paste(rep("+",25), collapse=''))
 print(paste0("the arguments: ", args))
+print(paste0('re-run: ', rerun_rd))
+
+for (i in seq(1:3)) {
+    Sys.sleep(1)
+    print(paste0(i, '...'))
+}
