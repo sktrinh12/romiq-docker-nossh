48a43567546e2e91af560bf4ac6c3d07f2bbe243
sktrinh12 <trinh.kcs85@gmail.com>
2021-01-30 16:16:37 -0800

    used fastapi BackgroundTasks instead of ThreadPoolExecutor

--- services/romiq/app.py
+++ services/romiq/app.py
@@ -1,16 +1,15 @@
-from fastapi import FastAPI, Request, HTTPException
+from fastapi import FastAPI, Request, HTTPException, BackgroundTasks
 from typing import Optional
 from functions import *
-from concurrent.futures import ThreadPoolExecutor, as_completed
+# from concurrent.futures import ThreadPoolExecutor, as_completed
 import socket
 import uvicorn
 
-executor = ThreadPoolExecutor(2)
 app = FastAPI()
 
 
 @app.get("/run/{script_name}/{barcode}")
-async def runOmiq(script_name: str, barcode: str, re_run: Optional[str] = 'false'):
+async def runOmiq(script_name: str, barcode: str, background_tasks: BackgroundTasks, re_run: Optional[str] = 'false'):
     # print(f'barcode: {barcode}')
     # print(f'script_name: {script_name}')
     if not validate_barcode(barcode):
@@ -19,13 +18,13 @@ async def runOmiq(script_name: str, barcode: str, re_run: Optional[str] = 'false
     if re_run:
         # print(f're_run: {re_run}')
         output.update({'re_run' : re_run})
-    processes = []
-    output = []
-    with ThreadPoolExecutor(max_workers=2) as TPExc:
-        TPExc.submit(run_Rscript, script_name, barcode=barcode, re_run=re_run)
+    # processes = []
+    # with ThreadPoolExecutor(max_workers=2) as TPExc:
+    #     TPExc.submit(run_Rscript, script_name, barcode=barcode, re_run=re_run)
         # processes.append(TPExc.submit(run_Rscript, script_name, barcode=barcode))
         # for _ in as_completed(processes):
         #     output.append(_.result())
+    background_tasks.add_task(run_Rscript, script_name, barcode=barcode, re_run=re_run)
     return output
 
 
